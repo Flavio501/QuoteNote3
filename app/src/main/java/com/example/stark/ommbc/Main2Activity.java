d9ec24b48971638096091e762fd2703242af5d06
Alejandro Del Villar <alejandro.delvillar@cetys.edu.mx>
2018-05-07 20:46:31 -0700

    added recyclerview view but need to fixx errors ADV

--- app/src/main/java/com/example/stark/ommbc/Main2Activity.java
+++ app/src/main/java/com/example/stark/ommbc/Main2Activity.java
@@ -2,14 +2,62 @@ package com.example.stark.ommbc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class Main2Activity extends AppCompatActivity {
+    private List<Quote> quoteList = new ArrayList<>();
+    private RecyclerView recyclerview;
+    private QuoteAdapter qAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerview = (RecyclerView) findViewById(R.id.recycler_view);
+
+        qAdapter = new QuoteAdapter(quoteList);
+        RecyclerView.LayoutManager qLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerview.setLayoutManager(qLayoutManager);
+        recyclerview.setItemAnimator(new DefaultItemAnimator());
+        recyclerview.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerview.setAdapter(qAdapter);
+
+        recyclerview.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerview, new RecyclerTouchListener.ClickListener(){
+            @Override
+            public void onClick(View view, int position){
+                Quote quote = quoteList.get(position);
+                Toast.makeText(getApplicationContext(), quote.getName() + "fue selecionado!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void on LongClick(View view, int position){
+                //add event for long click
+            }
+        }));
+
+        prepareQuoteData();
+
+    }
+
+    private void prepareQuoteData(){
+        //get quote info and displays to view
+
+        qAdapter.notifyDataSetChange();
     }
 }
---
+++ app/src/main/java/com/example/stark/ommbc/QuoteAdapter.java
@@ -0,0 +1,52 @@
+package com.example.stark.ommbc;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
+
+public class QuoteAdapter RecyclerView.Adapter<QuoteAdapter.MyViewHolder>{
+    private List<Quote> quoteList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView Name, timestamp, bidBig, bidPoints, offerBig, offerPoints, High, Low, Open;
+
+        public MyViewHolder(View view) {
+            super(view);
+            Name = (TextView) view.findViewById(R.id.Name);
+            timestamp = (TextView) view.findViewById(R.id.timestamp);
+            bidBig = (TextView) view.findViewById(R.id.bidBig);
+            bidPoints = (TextView) view.findViewById(R.id.bidPoints);
+            offerBig = (TextView) view.findViewById(R.id.offerBig);
+            offerPoints = (TextView) view.findViewById(R.id.offerPoints);
+            High = (TextView) view.findViewById(R.id.High);
+            Low = (TextView) view.findViewById(R.id.Low);
+            Open = (TextView) view.findViewById(R.id.Open);
+        }
+
+        public QuoteAdapter(List<Quote> quoteList){
+            this.quoteList = quoteList;
+        }
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position){
+            Quote q = quoteList.get(position);
+            holder.Name.setText(quote.getName());
+            holder.timestamp.setText(quote.gettimestamp());
+            holder.bidBig.setText(quote.getbidBig());
+            holder.bidPoints.setText(quote.getbidPoints);
+            holder.offerBig.setText(quote.getofferBig);
+            holder.offerPoints.setText(quote.getofferPoints());
+            holder.High.setText(quote.getHigh());
+            holder.Low.setText(quote.getLow());
+            hodler.Open.setText(quote.getOpen());
+        }
+
+        @Override
+        public int getItemCount(){
+            return quoteList.size();
+        }
+
+    }
+}
